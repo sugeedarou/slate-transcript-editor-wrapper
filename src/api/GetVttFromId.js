ebe45ce6abc79bb8de008d9935e3ccf4af5e36fb
omarjawabra <omarjawabra@gmail.com>
2022-01-19 14:29:16 +0100

    added get vtt api

---
+++ src/api/GetVttFromId.js
@@ -0,0 +1,27 @@
+/**
+ * log in api
+ * returns the token if login succeed
+ * returns false if login failed
+ */
+ export default async function GetVttFromId(token,id)
+ {
+     let response;
+     var myHeaders = new Headers();
+     myHeaders.append("Authorization", "Token "+token);
+     var formdata = new FormData();
+     formdata.append("taskId", id);
+     
+     var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+      };
+     
+     response = await fetch("https://i13hpc29.ira.uka.de:443/v1/getvtt/", requestOptions);
+ 
+     if(response.status==200)
+         return response.text()
+     return false;
+     
+ }
