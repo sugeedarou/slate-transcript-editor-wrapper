6c7ace06e4015aa279676a1bdb15eee5af12a5fe
omarjawabra <omarjawabra@gmail.com>
2022-01-21 23:26:22 +0100

    changed get vtt api to get the new response structure

--- src/api/GetVttFromId.js
+++ src/api/GetVttFromId.js
@@ -3,25 +3,29 @@
  * returns the token if login succeed
  * returns false if login failed
  */
- export default async function GetVttFromId(token,id)
- {
-     let response;
-     var myHeaders = new Headers();
-     myHeaders.append("Authorization", "Token "+token);
-     var formdata = new FormData();
-     formdata.append("taskId", id);
-     
-     var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: formdata,
-        redirect: 'follow'
-      };
-     
-     response = await fetch("https://i13hpc29.ira.uka.de:443/v1/getvtt/", requestOptions);
- 
-     if(response.status==200)
-         return response.text()
-     return false;
-     
- }
+export default async function GetVttFromId(token, id) {
+  let response;
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Token " + token);
+  var formdata = new FormData();
+  formdata.append("taskId", id);
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formdata,
+    redirect: "follow",
+  };
+
+  response = await fetch(
+    "https://i13hpc29.ira.uka.de:443/v1/getvtt/",
+    requestOptions
+  );
+
+  if (response.status == 200) {
+    let x = await response.json();
+    return x.vtt;
+  }
+
+  return false;
+}
