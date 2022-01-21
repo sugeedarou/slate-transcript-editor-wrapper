3c99d2b82aa561f4f7f5705c02def526d7f3727d
omarjawabra <omarjawabra@gmail.com>
2022-01-21 23:26:50 +0100

    deleted task id from set vtt correction api

--- src/api/SetVttCorrection.js
+++ src/api/SetVttCorrection.js
@@ -1,14 +1,12 @@
 import { getToken } from "../user/User";
 
- export default async function SetVttCorrection(task_id,vtt_name,vtt_content)
+ export default async function SetVttCorrection(vtt_name,vtt_content)
  {
      let response;
      var myHeaders = new Headers();
      let token = getToken()
      myHeaders.append("Authorization", "Token "+token);
      var formdata = new FormData();
-     if(task_id!="")
-        formdata.append("task_id",task_id );
     if(vtt_name!="")
         formdata.append("vtt_name", vtt_name);
     formdata.append("vtt", vtt_content);
