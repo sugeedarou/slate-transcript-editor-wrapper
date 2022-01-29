1ecd7b0eaf45b5c4ed7d1490eb003bd5566c7724
omarjawabra <omarjawabra@gmail.com>
2022-01-29 04:32:40 +0100

    created get user info api function

---
+++ src/api/GetUserInfo.js
@@ -0,0 +1,22 @@
+export default async function getUserInfo(token)
+ {
+     let response;
+     var myHeaders = new Headers();
+     myHeaders.append("Authorization", "Token "+token);
+     
+     var formdata = new FormData();
+     
+     var requestOptions = {
+       method: 'POST',
+       headers: myHeaders,
+       body: formdata,
+       redirect: 'follow'
+     };
+     
+     response = await fetch("https://i13hpc29.ira.uka.de:443/auth/userinfo/", requestOptions)
+     
+     if(response.status==200)
+         return response.json()
+     return false;
+     
+ }
